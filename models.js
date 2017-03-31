b07bd7fdf1b8f3d3786a26cfac8c55c66949f528
Chris Yang <ysm0622@gmail.com>
2017-03-31 11:35:56 +0900

    commit

---
+++ models.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var SequencesSchema = Schema({
+    _id: { type: String, required: true },
+    seq: { type: Number, default: 0 }
+});
+
+var sequences = mongoose.model('sequences', SequencesSchema);
+
+var UrlsSchema = new Schema({
+  _id: {type: Number, index: true},
+  url: String,
+  created_at: Date
+});
+
+UrlsSchema.pre('save', function(next){
+  var self = this;
+  sequences.findOneAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} }, {upsert: true}, function(error, result) {
+    console.log(result);
+    if (error) return next(error);
+    self.created_at = new Date();
+    self._id = result.seq;
+    next();
+  });
+});
+
+var urls = mongoose.model('urls', UrlsSchema);
+
+module.exports = urls;
--- server.js
+++ server.js
@@ -1,6 +1,51 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bijective = require('./bijective.js');
+var Urls = require('./models');
+
+mongoose.connect('mongodb://localhost/url-shortener');
+
 var app = express();
 app.use(express.static('public'));
+
+app.get('/url/:longUrl', function(req, res){
+
+  var shortUrl = '';
+
+  Urls.findOne({url: req.params.longUrl}, function (err, doc){
+    if (doc){
+      res.send({'key': doc._id});
+    } else {
+
+      var newUrl = Urls({
+        url: req.params.longUrl
+      });
+
+      newUrl.save(function(err) {
+        if (err) console.log(err);
+
+        res.send({'key': bijective.encode(newUrl._id)});
+      });
+    }
+
+  });
+
+});
+
+app.get('/:key', function(req, res){
+
+  var id = bijective.decode(req.params.key);
+
+  Urls.findOne({_id: id}, function (err, doc){
+    if (doc) {
+      res.redirect(doc.url);
+    } else {
+      res.redirect("/");
+    }
+  });
+
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
